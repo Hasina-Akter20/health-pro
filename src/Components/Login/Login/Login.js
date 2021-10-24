89e37fb910ef811b75d81aed4e8631f2ecebfd18
Hasina Akter <66660517+Hasina-Akter20@users.noreply.github.com>
2021-10-24 16:44:59 +0600

    almost completed

--- src/Components/Login/Login/Login.js
+++ src/Components/Login/Login/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { googleSignIn, error, facebookSignIn, setError } = useAuth();
-  // console.log(email, password);
+
   const auth = getAuth();
 
   // signin with email and password
